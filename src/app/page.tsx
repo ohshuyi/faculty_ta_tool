edbed09922dd9e14202525a2c4007255050c73e7
Bryan Tay <rivenbryan@gmail.com>
2024-10-22 14:07:06 +0800

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,40 @@
-import Image from "next/image";
+"use client"
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Fetch data from the API
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/users");
+        const result = await response.json();
+        // Set the first result to state if it exists
+        if (result.length > 0) {
+          setData(result[0]);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array to run the effect once on mount
+
   return (
     <div>
-      hello
+      <h1>Hello</h1>
+      {data ? (
+        <div>
+          {/* @ts-ignore */}
+          <p>First User Name: {data.name}</p>
+          {/* @ts-ignore */}
+          <p>First User Email: {data.email}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
