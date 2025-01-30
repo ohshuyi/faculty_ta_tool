5afa056f7aa0dc7c3cb63ba24753373e2d2c311f
rivenbryan <rivenbryan@gmail.com>
2025-01-31 01:14:14 +0800

    test

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -64,7 +64,7 @@ const AppLayout = ({ children }) => {
           justifyContent: "space-between",
         }}
       >
-        <Image src="/ntu.png" width={160} height={10} alt="NTU" />
+        {/* <Image src="/ntu.png" width={160} height={10} alt="NTU" /> */}
 
         <Menu
           theme="dark"
